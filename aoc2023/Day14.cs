eb2440aeb3d92c5206bceac45a223d3532e702f6
bengto <bengto@axis.com>
2023-12-15 06:43:56 +0100

    Day 15 done

--- aoc2023/Day14.cs
+++ aoc2023/Day14.cs
@@ -9,39 +9,224 @@ namespace aoc2023
 {
     internal class Day14
     {
+        internal const long HashSize = 16387000000;
+
+        internal long Hash(ref char[][] board)
+        {
+            long sum = 0;
+
+            for (int r = 1; r < board.Length - 1; r++)
+            {
+                for (int k = 1; k < board[0].Length - 1; k++)
+                {
+                    if (board[r][k] == 'O')
+                    {
+                        sum += r * board.Length + k;
+                    }
+                }
+
+            }
+
+            return sum % HashSize;
+        }
+
+        internal void Tilt(ref char[][] board, int direction = 0)
+        {
+            if (direction == 0) // up
+            {
+                for (int i = 1; i < board[0].Length - 1; i++)
+                {
+                    for (int r = 1; r < board.Length - 1; r++)
+                    {
+                        if (board[r][i] == 'O')
+                        {
+                            int p = r;
+
+                            while (board[p - 1][i] == '.')
+                            {
+                                board[p - 1][i] = 'O';
+                                board[p][i] = '.';
+                                p--;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (direction == 1) // left
+            {
+                for (int r = 1; r < board.Length - 1; r++)
+                {
+                    for (int i = 1; i < board[0].Length - 1; i++)
+                    {
+                        if (board[r][i] == 'O')
+                        {
+                            int p = i;
+
+                            while (board[r][p - 1] == '.')
+                            {
+                                board[r][p - 1] = 'O';
+                                board[r][p] = '.';
+                                p--;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (direction == 2) // down
+            {
+                for (int i = 1; i < board[0].Length - 1; i++)
+                {
+                    for (int r = board.Length - 1; r >= 1; r--)
+                    {
+                        if (board[r][i] == 'O')
+                        {
+                            int p = r;
+
+                            while (board[p + 1][i] == '.')
+                            {
+                                board[p + 1][i] = 'O';
+                                board[p][i] = '.';
+                                p++;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (direction == 3) // right
+            {
+                for (int r = 1; r < board.Length - 1; r++)
+                {
+                    for (int i = board[0].Length - 1; i >= 1; i--)
+                    {
+                        if (board[r][i] == 'O')
+                        {
+                            int p = i;
+
+                            while (board[r][p + 1] == '.')
+                            {
+                                board[r][p + 1] = 'O';
+                                board[r][p] = '.';
+                                p++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int Weight(ref char[][] board)
+        {
+            int sum = 0;
+            var rowval = board.Length - 2;
+
+            for (int r = 1; r < board.Length - 1; r++)
+            {
+                for (int i = 1; i < board[0].Length - 1; i++)
+                {
+                    if (board[r][i] == 'O')
+                    {
+                        sum += rowval;
+                    }
+                }
+
+                rowval--;
+            }
+
+            return sum;
+        }
+
+
         public void Part1()
         {
             var data = File.ReadAllLines(@"data\day14.txt");
 
-            List<int> cals = new List<int>();
+            var values = ArrayMethods.AddBorder(1, '#', data).Select(s => s.Select(c => c).ToArray()).ToArray();
 
-            var values = data.Select(r => r.Split(' ')).ToArray();
-
-            /*
-            var values = data.Select(r => r.Length == 0 ? -1 : Int32.Parse(r)).ToArray();
-
-            var values = data.Select(r => r.Select(c => (int)(c - 'a')).ToArray()).ToArray();
-
-            var values = data.Select(r => Int64.Parse(r)).ToArray();
-            */
+            Tilt(ref values, 0);
 
 
+            var sum = Weight(ref values);
 
-            Console.WriteLine($"Answer is {values.Count()}");
+
+            Console.WriteLine($"Answer is {sum}");
         }
 
         public void Part2()
         {
             var data = File.ReadAllLines(@"data\day14.txt");
 
-            List<int> cals = new List<int>();
+            /*
+            data = new[]
+            {
+                "O....#....",
+                "O.OO#....#",
+                ".....##...",
+                "OO.#O....O",
+                ".O.....O#.",
+                "O.#..O.#.#",
+                "..O..#O..O",
+                ".......O..",
+                "#....###..",
+                "#OO..#....",
+            };
+            */
 
-            var values = data.Select(r => r.Split(' ')).ToArray();
+            var values = ArrayMethods.AddBorder(1, '#', data).Select(s => s.Select(c => c).ToArray()).ToArray();
+
+            var hashes = new List<long>();
+
+            for (long c = 0; c < 2000; c++)
+            {
+                for (int d = 0; d < 4; d++)
+                {
+                    Tilt(ref values, d);
+                }
+
+                hashes.Add(Hash(ref values));
+            }
+
+            // Find period
+            int skip = 1000;
+            var periods = new List<long>();
+
+            for (int p = 2; p < 150 + 2; p++)
+            {
+                var pp = 3*hashes[skip] - hashes[skip + p] - hashes[skip + 2*p] - hashes[skip + 3*p];
+                periods.Add(pp);
+            }
+
+            var period = 0;
+            while (period < periods.Count && periods[period] != 0)
+            {
+                period++;
+            }
+
+            period += 2; // It's offsetted...
+
+            var periodsString = string.Join(" ", periods.Select(v => v.ToString()).ToArray());
+            Console.WriteLine($"Periods: {periodsString}");
+            Console.WriteLine($"Detected period = {period}");
+
+            // Calculate number of cycles to 1000000000, reduced
+
+            var nCycles = ((1000000000 - skip) % period) + skip;
+
+            // Redo the whole stuff
+
+            values = ArrayMethods.AddBorder(1, '#', data).Select(s => s.Select(c => c).ToArray()).ToArray();
+
+            for (long c = 0; c < nCycles; c++)
+            {
+                for (int d = 0; d < 4; d++)
+                {
+                    Tilt(ref values, d);
+                }
+
+                //Console.WriteLine($"Weight: {Weight(ref values)}");
+            }
 
 
-
-
-            Console.WriteLine($"Answer is {values.Count()}");
+            Console.WriteLine($"Answer is {Weight(ref values)}");
         }
 
     }
--- aoc2023/Day15.cs
+++ aoc2023/Day15.cs
@@ -7,41 +7,98 @@ using System.Threading.Tasks;
 
 namespace aoc2023
 {
+    internal class Lens
+    {
+        public string Id { get; }
+        public int Value { get; set; }
+        public int Box { get; }
+
+        public Lens(string id, int value)
+        {
+            Id = id;
+            Value = value;
+            Box = id.Aggregate(0, (a, b) => ((a + b) * 17) % 256);
+        }
+    }
+
     internal class Day15
     {
+        internal long Hash(string s)
+        {
+            return s.Aggregate((long)0, (a, c) => ((a + (long)c) * 17) % 256);
+        }
+
         public void Part1()
         {
             var data = File.ReadAllLines(@"data\day15.txt");
 
-            List<int> cals = new List<int>();
+            var values = data.Select(r => r.Split(',').ToArray()).ToArray();
 
-            var values = data.Select(r => r.Split(' ')).ToArray();
+            long sum = 0;
 
-            /*
-            var values = data.Select(r => r.Length == 0 ? -1 : Int32.Parse(r)).ToArray();
+            sum = values[0].Select(v => Hash(v)).Sum();
 
-            var values = data.Select(r => r.Select(c => (int)(c - 'a')).ToArray()).ToArray();
-
-            var values = data.Select(r => Int64.Parse(r)).ToArray();
-            */
-
-
-
-            Console.WriteLine($"Answer is {values.Count()}");
+            
+            Console.WriteLine($"Answer is {sum}");
         }
+
+        internal List<Lens>[] Boxes = new List<Lens>[256];
 
         public void Part2()
         {
             var data = File.ReadAllLines(@"data\day15.txt");
 
-            List<int> cals = new List<int>();
+            var values = data.Select(r => r.Split(',').ToArray()).ToArray();
 
-            var values = data.Select(r => r.Split(' ')).ToArray();
+            for (int i = 0; i < 256; i++)
+            {
+                Boxes[i] = new List<Lens>();
+            }
+
+            foreach (var val in values[0])
+            {
+                if (val[val.Length - 1] == '-')
+                {
+                    var id = val.Substring(0, val.Length - 1);
+                    var boxId = Hash(id);
+
+                    Boxes[boxId].RemoveAll(lens => lens.Id == id);
+                }
+                else
+                {
+                    int focusVal = val[val.Length - 1] - '0';
+                    if (val[val.Length - 2] != '=')
+                    {
+                        throw new Exception("klsdlfls");
+                    }
+
+                    var id = val.Substring(0, val.Length - 2);
+
+                    var lens = new Lens(id, focusVal);
+
+                    if (Boxes[lens.Box].Any(l => l.Id == lens.Id))
+                    {
+                        Boxes[lens.Box].First(l => l.Id == lens.Id).Value = lens.Value;
+                    }
+                    else
+                    {
+                        Boxes[lens.Box].Add(lens);
+                    }
+                }
+            }
+
+            long sum = 0;
+
+            for (int box = 0; box < 256; box++)
+            {
+                for (int lens = 0; lens < Boxes[box].Count; lens++)
+                {
+                    sum += (box + 1) * (lens + 1) * Boxes[box][lens].Value;
+                }
+            }
 
 
-
-
-            Console.WriteLine($"Answer is {values.Count()}");
+            Console.WriteLine($"Answer is {sum}");
         }
 
     }
